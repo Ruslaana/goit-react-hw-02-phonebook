436de86b7c53210918a00493e7103e17d45e1430
Ruslaana Onyshchuk <lalelya7@gmail.com>
2023-06-26 11:03:39 +0200

    task 2

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyledSection } from './Section.styled';
+
+function Section({ title, children }) {
+  return (
+    <StyledSection>
+      <h2 className="section-title">{title}</h2>
+      <div className="section-content">{children}</div>
+    </StyledSection>
+  );
+}
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Section;
